6f85bf31ce04248db370d48e9d28f949037ca3d4
sumersingla <sumersingla14@gmail.com>
2024-07-15 11:28:31 +0530

    Adding attachment chooser functionality

--- src/main/java/client/EmailSender.java
+++ src/main/java/client/EmailSender.java
@@ -2,21 +2,22 @@ package client;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.File;
 import java.util.Properties;
 
 public class EmailSender implements IEmailSender {
+    private final String username = "sumersingla14@gmail.com";
+    private final String password = "gwjk uqlm fcfk jgof";
+    private final Properties smtpProps = new Properties();
+
     public void sendEmailViaGmail(String to, String subject, String body) {
-        final String username = "sumersingla14@gmail.com";
-        final String password = "gwjk uqlm fcfk jgof";
 
-        Properties props = new Properties();
-        props.setProperty("mail.smtp.host", "smtp.gmail.com");
-        props.setProperty("mail.smtp.port", "587");
-        props.setProperty("mail.smtp.auth", "true");
-        props.setProperty("mail.smtp.starttls.enable", "true");
+        getSMTPProperties(smtpProps);
 
-        Session session = Session.getInstance(props, new Authenticator() {
+        Session session = Session.getInstance(smtpProps, new Authenticator() {
         protected PasswordAuthentication getPasswordAuthentication() {
             return new PasswordAuthentication(username, password);
         }});
@@ -31,8 +32,53 @@ public class EmailSender implements IEmailSender {
             Transport.send(mimeMessage);
             System.out.println("Email sent successfully via Gmail");
         } catch (MessagingException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
 
+    }
+
+    /* Each file selected for attachment is added as a separate MimeBodyPart,
+    and all parts (text and attachments) are combined into a Multipart object. */
+    public void sendEmailViaGmailWithAttachments(String to, String subject, String body, File[] attachments){
+        getSMTPProperties(smtpProps);
+
+        Session session = Session.getInstance(smtpProps, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }});
+
+        try {
+            Message mimeMessage = new MimeMessage(session);
+            mimeMessage.setFrom(new InternetAddress(username));
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            mimeMessage.setSubject(subject);
+            Multipart multipart = new MimeMultipart();
+
+            MimeBodyPart textPart = new MimeBodyPart();
+            textPart.setText(body);
+            multipart.addBodyPart(textPart);
+
+            for (File attachment : attachments) {
+                MimeBodyPart attachmentPart = new MimeBodyPart();
+                try {
+                    attachmentPart.attachFile(attachment);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            mimeMessage.setContent(multipart);
+            Transport.send(mimeMessage);
+            System.out.println("Email sent successfully via Gmail w Attachments");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getSMTPProperties(Properties props) {
+        props.setProperty("mail.smtp.host", "smtp.gmail.com");
+        props.setProperty("mail.smtp.port", "587");
+        props.setProperty("mail.smtp.auth", "true");
+        props.setProperty("mail.smtp.starttls.enable", "true");
     }
 }
--- src/main/java/client/IEmailSender.java
+++ src/main/java/client/IEmailSender.java
@@ -1,5 +1,9 @@
 package client;
 
+import java.io.File;
+
 public interface IEmailSender {
     void sendEmailViaGmail(String to, String subject, String body);
-}
+    void sendEmailViaGmailWithAttachments(String to, String subject, String body, File[] attachments);
+
+    }
